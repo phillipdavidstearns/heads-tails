2bed6dedd4e1af823cd7348e8eae9a02b7d9151b
phillipdavidstearns <phil@phillipstearns.com>
2019-12-30 00:03:20 -0500

    playing with hardware PWM

--- Python/pigpioPWM.py
+++ Python/pigpioPWM.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
 import pigpio
-import os
 import signal
 import time
 import math
-import subprocess
 
 angle = 0.0
 angleInc = 0.01
 
-# os.system('sudo /usr/bin/pigpiod')
-subprocess.call(['/bin/bash', '-c','sudo /usr/bin/pigpiod'])
+
 
 PWM = pigpio.pi()
 
@@ -19,14 +16,15 @@ def keyboardInterruptHandler(signal, frame):
 	print()
 	print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
 	PWM.hardware_PWM(12, 400, 0)
-	os.system('sudo killall pigpiod')
-	subprocess.call(['/bin/bash', '-c', 'sudo killall pigpiod'])
+	PWM.stop()
 	exit(0)
 
 def main():
+	global angle
 	while True:
-		PWM.hardware_PWM(12, 400, 0)
+		PWM.hardware_PWM(12, 400, int(1000000 * pow(math.sin(2*math.pi*angle),2)))
 		time.sleep(1/30)
+		angle += angleInc
 
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
