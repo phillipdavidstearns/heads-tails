c2ff87eb9f562889c12852867d0c52e9f7d7d88d
phillipdavidstearns <phil@phillipstearns.com>
2019-12-29 23:46:46 -0500

    playing with hardware PWM

---
+++ Python/pigpioPWM.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import pigpio
+import os
+import signal
+import time
+import math
+
+angle = 0.0
+angleInc = 0.01
+
+os.system('/usr/bin/pigpiod')
+PWM = pigpio.pi()
+
+def keyboardInterruptHandler(signal, frame):
+	print()
+	print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
+	PWM.hardware_PWM(12, 400, 0)
+	os.system('killall pigpiod')
+	exit(0)
+
+def main():
+	while True:
+		PWM.hardware_PWM(12, 400, 1000000 * pow( math.sin( 2*math.pi*angle),2))
+		sleep(1/30)
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+setup()
+main()
