3122dd5fc6f7bd0c439d40779a8059c129da4cb4
phillipdavidstearns <phil@phillipstearns.com>
2020-01-03 16:53:28 -0500

    max number of threads supported by Python?

--- Python/maxThreads.py
+++ Python/maxThreads.py
@@ -5,13 +5,15 @@ from threading import Timer
 count = 0
 
 def callback(number):
+	global count
+	count -= 1
 	print(" [-] Thread finished: "+str(number), end='\r')
 
 def main():
 	global count
 	while True:
 		count+=1
-		Timer(10, callback,[count])
-		print(" [+] Thread started: "+str(count), end='\r')
+		Timer(120, callback,[count])
+		print(" [+] Threads running: "+str(count), end='\r')
 
 main()
