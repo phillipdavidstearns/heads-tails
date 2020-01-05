ee92557b46015e354c72f78d6f95677443bc7959
phillipdavidstearns <phil@phillipstearns.com>
2020-01-05 17:54:56 -0500

    using polling timers instead of threaded timers

--- Python/csvTiming.py
+++ Python/csvTiming.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3/
 
 # csv tab of the score is publicly available at:
 # https://docs.google.com/spreadsheets/d/1IF0b8Fv-7jCC3OciHavgOJIZhVEpCWoEPLl8GdaNXFA/edit#gid=1797776547
@@ -7,21 +7,34 @@
 # (credit - https://stackoverflow.com/questions/24255472/download-export-public-google-spreadsheet-as-tsv-from-command-line)
 
 import csv
-from threading import Timer
 import random
 import time
 import random
 import signal
 import os
 
-t0 = 0.0
 update_score=False
 CHANNELS=32
 channelStates=[]
+eventTimes=[]
+eventIndexes=[]
+lastCycleTime = 0
 
-for i in range(CHANNELS):
-	channelStates.append(0)
-	
+def clearEventTimes():
+	global eventTimes
+	for i in range(CHANNELS):
+		eventTimes.append([])
+
+def clearChannelStates():
+	global channelStates
+	for i in range(CHANNELS):
+		channelStates.append(0)
+
+def clearEventIndexes():
+	global eventIndexes
+	for i in range(CHANNELS):
+		eventIndexes.append(0)
+
 script_dir = os.path.split(os.path.realpath(__file__))[0]
 curl = 'curl --connect-timeout 5 -m 10 -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGp8GI85wmWP7yZaUa0EV_reKdn2yDFgRBotHnqVOfPKjek4_6JIy4lCnnp9xT9BZavKjeOy-ZYsn_/pub?gid=1797776547&single=true&output=csv"'
 temp_filename = "\"" + script_dir + "/data/score_temp.csv" +  "\""
@@ -31,12 +44,10 @@ cmd = curl+" > "+temp_filename
 def setLightOn(channel):
 	global channelStates
 	channelStates[channel] = 1
-	# print("Channel: " + str(channel) + ", Light is ON: " + str(time.time() - t0))
 
 def setLightOff(channel):
 	global channelStates
 	channelStates[channel] = 0
-	# print("Channel: " + str(channel) + ", Light is OFF " + str(time.time() - t0))
 
 def updateCSV():
 	update = -1
@@ -90,37 +101,60 @@ def printBehavior(behavior):
 	for i, timing in enumerate(behavior):
 		print(behavior[i])
 
-def createTimers(behavior, whichChannel=0):
-	timers=[]
-	offset = random.uniform(-behavior[2],behavior[2])
-	for i in range(len(behavior[0])):
-		delay = offset + behavior[0][i] + random.uniform(-behavior[1][i],behavior[1][i])
-		if (i % 2 == 0):
-			timers.append(Timer(delay,setLightOn,[whichChannel]))
-		else:
-			timers.append(Timer(delay,setLightOff,[whichChannel]))
-	return timers
+def timing():
 
-def initiateTimers(timers):
-	global t0
-	t0 = time.time()
-	for t in timers:
-		t.start()
+	global eventTimes
+	global eventIndexes
+
+	for c in range(CHANNELS):
+		if eventTimes[c]:
+			if (time.time() > eventTimes[c][0]):
+				eventIndexes[c]^=1
+				if (eventIndexes[c] == 1):
+					setLightOn(c)
+				elif (eventIndexes[c] == 0):
+					setLightOff(c)
+				eventTimes[c]=eventTimes[c][1:]
+				if (len(eventTimes[c])==0):
+					setLightOff(c)
+					eventIndexes[c]^=1
+
+
+def generateTimings(behavior):
+	times=[]
+	startTime=time.time()
+	offset = random.uniform(-behavior[2],behavior[2])
+	for t in range(len(behavior[0])):
+		eventTime = startTime + offset + behavior[0][t] + random.uniform(-behavior[1][t],behavior[1][t])
+		times.append(eventTime)
+	return times
 
 def main():
+
+	clearEventTimes()
+	clearChannelStates()
+	clearEventIndexes()
 
 	if update_score: updateCSV()
 
 	behaviors = openCSV()
 
+	global eventIndexes
+	global eventTimes
+	global lastCycleTime
+
 	while True:
-		cylceTime = int(time.time()) % 90 
-		print(str(cylceTime)+str(channelStates),end='\r')
-		if( cylceTime == 0):
-			for i in range(CHANNELS):
+
+		cycleTime = int(time.time()) % 90 
+		print("--->"+str(cycleTime)+str(channelStates),end='\r')
+		if( cycleTime == 0 and cycleTime != lastCycleTime):
+			startTime = time.time()
+			for c in range(CHANNELS):
 				index = random.randint(0,len(behaviors)-1)
 				behavior = behaviors[ index ]
-				initiateTimers(createTimers(behavior, i))
+				eventTimes[c]+=generateTimings(behavior)
+		timing()
+		lastCycleTime=cycleTime
 		time.sleep(.1)
 
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
--- Python/heads_tails.py
+++ Python/heads_tails.py
@@ -7,7 +7,6 @@
 # (credit - https://stackoverflow.com/questions/24255472/download-export-public-google-spreadsheet-as-tsv-from-command-line)
 
 import csv
-from threading import Timer
 import random
 import time
 import signal
@@ -58,13 +57,26 @@ def regOutput(channels):
 # Behavior related
 
 update_score=False
-last_cycle=0
 CHANNELS=32
 channelStates=[]
-timer_count = 0
+eventTimes=[]
+eventIndexes=[]
+lastCycleTime = 0
 
-for i in range(CHANNELS):
-	channelStates.append(0)
+def clearEventTimes():
+	global eventTimes
+	for i in range(CHANNELS):
+		eventTimes.append([])
+
+def clearChannelStates():
+	global channelStates
+	for i in range(CHANNELS):
+		channelStates.append(0)
+
+def clearEventIndexes():
+	global eventIndexes
+	for i in range(CHANNELS):
+		eventIndexes.append(0)
 
 script_dir = os.path.split(os.path.realpath(__file__))[0]
 curl = 'curl --connect-timeout 5 -m 10 -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGp8GI85wmWP7yZaUa0EV_reKdn2yDFgRBotHnqVOfPKjek4_6JIy4lCnnp9xT9BZavKjeOy-ZYsn_/pub?gid=1797776547&single=true&output=csv"'
@@ -72,19 +84,13 @@ temp_filename = "\"" + script_dir + "/data/score_temp.csv" +  "\""
 filename = "\"" + script_dir + "/data/score.csv" +  "\""
 cmd = curl+" > "+temp_filename
 
-
 def setLightOn(channel):
 	global channelStates
 	channelStates[channel] = 1
-	global timer_count
-	timer_count -= 1
-
 
 def setLightOff(channel):
 	global channelStates
 	channelStates[channel] = 0
-	global timer_count
-	timer_count -= 1
 
 def updateCSV():
 	update = -1
@@ -134,22 +140,33 @@ def printBehavior(behavior):
 	for i, timing in enumerate(behavior):
 		print(behavior[i])
 
-def createTimers(behavior, whichChannel=0):
-	timers=[]
-	offset = random.uniform(-behavior[2],behavior[2])
-	for i in range(len(behavior[0])):
-		delay = offset + behavior[0][i] + random.uniform(-behavior[1][i],behavior[1][i])
-		if (i % 2 == 0):
-			timers.append(Timer(delay,setLightOn,[whichChannel]))
-		else:
-			timers.append(Timer(delay,setLightOff,[whichChannel]))
-	return timers
+def timing():
 
-def initiateTimers(timers):
-	global timer_count
-	for t in timers:
-		t.start()
-		timer_count += 1
+	global eventTimes
+	global eventIndexes
+
+	for c in range(CHANNELS):
+		if eventTimes[c]:
+			if (time.time() > eventTimes[c][0]):
+				eventIndexes[c]^=1
+				if (eventIndexes[c] == 1):
+					setLightOn(c)
+				elif (eventIndexes[c] == 0):
+					setLightOff(c)
+				eventTimes[c]=eventTimes[c][1:]
+				if (len(eventTimes[c])==0):
+					setLightOff(c)
+					eventIndexes[c]^=1
+
+
+def generateTimings(behavior):
+	times=[]
+	startTime=time.time()
+	offset = random.uniform(-behavior[2],behavior[2])
+	for t in range(len(behavior[0])):
+		eventTime = startTime + offset + behavior[0][t] + random.uniform(-behavior[1][t],behavior[1][t])
+		times.append(eventTime)
+	return times
 
 def keyboardInterruptHandler(signal, frame):
 	print()
@@ -161,23 +178,34 @@ def keyboardInterruptHandler(signal, frame):
 	os._exit(0)
 
 def main():
-	
+
 	regClear()
+
+	clearEventTimes()
+	clearChannelStates()
+	clearEventIndexes()
+
 	if update_score: updateCSV()
 
 	behaviors = openCSV()
 
+	global eventIndexes
+	global eventTimes
+	global lastCycleTime
+
 	while True:
+
 		cycleTime = int(time.time()) % 90 
-		print("----| " + str(cycleTime)+str(channelStates)+", active timers: " + str(timer_count))
-		if( cycleTime == 0 and cycleTime != last_cycle ):
-			print("I should trigger only once!")
-			for i in range(CHANNELS):
+		print("--->"+str(cycleTime)+str(channelStates),end='\r')
+		if( cycleTime == 0 and cycleTime != lastCycleTime):
+			startTime = time.time()
+			for c in range(CHANNELS):
 				index = random.randint(0,len(behaviors)-1)
 				behavior = behaviors[ index ]
-				initiateTimers(createTimers(behavior, i))
-		last_cycle = cycleTime
+				eventTimes[c]+=generateTimings(behavior)
+		timing()
 		regOutput(channelStates)
+		lastCycleTime=cycleTime
 		time.sleep(1/FPS)
 
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
