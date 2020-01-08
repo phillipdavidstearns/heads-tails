31facfae8a7217a2d9781cd6aa182ab3673e80a1
phillipdavidstearns <phil@phillipstearns.com>
2020-01-08 16:34:50 -0500

    quick updates

--- Python/update.sh
+++ Python/update.sh
@@ -2,6 +2,7 @@
 
 git add -A
 git commit -am "quick updates"
+git push
 ssh heads-tails-01 'cd /home/heads-tails/heads-tails;git pull'
 ssh heads-tails-02 'cd /home/heads-tails/heads-tails;git pull'
 exit 0
