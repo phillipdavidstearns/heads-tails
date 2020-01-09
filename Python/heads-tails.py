775566504469a97885c254f46d289a758f2a540f
phillipdavidstearns <phil@phillipstearns.com>
2020-01-09 17:54:40 -0500

    quick updates

--- Python/heads-tails.py
+++ Python/heads-tails.py
@@ -16,7 +16,7 @@ FPS = 30
 INCREMENT = 1/120.0
 
 tzOffset = -5 * 3600
-dotOffset = 0 # based on the start of Phase B @ 51 seconds in the cycle starting + 28 past midnight
+dotOffset = -26 # based on the start of Phase B @ 51 seconds in the cycle starting + 28 past midnight
 deviation = 0
 power_line_time=time.time()
 
