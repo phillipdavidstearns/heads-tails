b82f62ee0086cdd054749128ebea7c9f75bdb5db
phillipdavidstearns <phil@phillipstearns.com>
2020-01-09 16:33:52 -0500

    quick updates

--- Python/gpioHandlers.py
+++ Python/gpioHandlers.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO # using RPi.GPIO for non-PWM
 import subprocess
 import time
 
-def incrementCounter(channel, var, inc):
+def incrementCounter(channel):
 	global power_line_time
 	power_line_time += INCREMENT
 	print("power_line_time: "+str(power_line_time),end='\r')
