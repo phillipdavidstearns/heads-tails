0ec1b969b2ac02c857c67dc9c9b7bcf1f185ecc0
phillipdavidstearns <phil@phillipstearns.com>
2020-01-09 16:48:16 -0500

    quick updates

--- Python/globalVars.py
+++ Python/globalVars.py
@@ -3,33 +3,33 @@ import os
 import pigpio
 
 
-	CHANNELS=32
-	FPS = 30
+CHANNELS=32
+FPS = 30
 
-	INCREMENT = 1/120.0
+INCREMENT = 1/120.0
 
-	tzOffset = -5 * 3600
-	dotOffset = 0 # based on the start of Phase B @ 51 seconds in the cycle starting + 28 past midnight
-	deviation = 0
-	power_line_time=time.time()
+tzOffset = -5 * 3600
+dotOffset = 0 # based on the start of Phase B @ 51 seconds in the cycle starting + 28 past midnight
+deviation = 0
+power_line_time=time.time()
 
-	channelStates=[]
-	eventTimes=[]
-	eventIndexes=[]
-	lastCycleTime = 0
+channelStates=[]
+eventTimes=[]
+eventIndexes=[]
+lastCycleTime = 0
 
-	script_dir = os.path.split(os.path.realpath(__file__))[0]
+script_dir = os.path.split(os.path.realpath(__file__))[0]
 
 
-	STR = 17
-	DATA = 27
-	CLK = 22
-	GRID = 23
+STR = 17
+DATA = 27
+CLK = 22
+GRID = 23
 
-	PWM_PIN = 12
-	PWM_FREQ = 14000 # frequency of PWM
+PWM_PIN = 12
+PWM_FREQ = 14000 # frequency of PWM
 
-	PWM = pigpio.pi()
+PWM = pigpio.pi()
 
 def init():
 	global CHANNELS
